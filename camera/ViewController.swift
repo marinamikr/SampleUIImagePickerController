a33d4a90c21fdf6a733ad6a75e0c1c32b7a59cfa
Marina <32869115+marinamikr@users.noreply.github.com>
2019-01-08 17:52:21 +0900

    Initial Commit

---
+++ camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  camera
+//
+//  Created by 原田摩利奈 on 2019/01/08.
+//  Copyright © 2019 原田摩利奈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
